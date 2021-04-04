746cebc8f31c8dc10527e6190a9120add79b27e0
Nikita <80910953+Seryoto@users.noreply.github.com>
2021-04-05 00:39:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+ $(".photo").css("background-color", "white")
+  $(".photo").css("color", "black")
+
+$(".photo").click(function(){
+  $(".portfolioChildBlock").attr("src", "img/1.jpg")
+  $(".photo").css("background-color", "white")
+  $(".photo").css("color", "black")
+   
+  $(".graphic").css("background-color", "black")
+  $(".graphic").css("color", "white")
+  
+  
+  $(".web").css("background-color", "black")
+  $(".web").css("color", "white")
+  
+  
+});
+  
+$(".graphic").click(function () {
+ 
+   $(".photo").css("background-color", "black")
+   $(".photo").css("color", "white")
+    
+   $(".graphic").css("background-color", "white")
+   $(".graphic").css("color", "black")
+   
+   $(".portfolioChildBlock").attr("src", "img/2.jpg")
+  
+   $(".web").css("background-color", "black")
+   $(".web").css("color", "white")
+  
+});
+
+$(".web").click(function () {
+  
+  $(".portfolioChildBlock").attr("src", "img/3.jpg")
+  
+  $(".web").css("background-color", "white")
+  $(".web").css("color", "black")
+   
+  $(".graphic").css("background-color", "black")
+  $(".graphic").css("color", "white")
+  
+  $(".photo").css("background-color", "black")
+  $(".photo").css("color", "white")
+  
+  
+});
+
