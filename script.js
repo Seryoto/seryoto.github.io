c65e6735279b6a8bbaf2591ce96d75062f76ad04
Nikita <80910953+Seryoto@users.noreply.github.com>
2021-05-06 18:03:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+$("location").ready(
+    function(){
+        $(".head-bar").animate({
+            "top": "25px"
+        }, 500);
+
+        $(".head-bar").animate({
+            "top": "20px"
+        }, 200);
+
+        $(".prewiew").animate({
+            "margin-left": "160px",
+        }, 800);
+
+        $(".prewiew").animate({
+            "margin-left": "150px",
+        }, 200);
+        $(".art-img-cont").fadeIn("500");
+    }
+
+)
