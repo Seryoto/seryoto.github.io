86a13a891259f2e51c21012c2121e25802d4a767
Seryoto <80910953+Seryoto@users.noreply.github.com>
2022-01-07 20:42:16 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -14,16 +14,50 @@ navBtnBlock.addEventListener('mouseout', (event)=>{
 
 
   event.target.classList.remove('active-nav-btn');
-  navBtn.classList.add('active-nav-btn');
 
+  navBtn.classList.add('active-nav-btn');
 
 });
 
+//setInterval(()=>{
+//  console.log(window.scrollY);
+// 100);
+
+
+//nav on scroll switch
+
+console.log(window.scrollY);
+
+window.addEventListener('scroll', ()=>{
+  if(window.scrollY > '110'){
+  navBtn.classList.remove('active-nav-btn');
+  navBtnBlock.children[1].classList.add('active-nav-btn');
+} else if(window.scrollY < '110'){
+  navBtnBlock.children[1].classList.remove('active-nav-btn');
+  navBtn.classList.add('active-nav-btn');
+}
+});
+
+
+
+
+
 //glass movement
 
-const glassPath = document.querySelector('.cls-1');
+/*const glassPath = document.querySelector('.cls-1');
 const glassSvg = document.querySelector('.land-svg');
 
 glassPath.addEventListener('mouseover',(event)=>{
   glassSvg.style.animation = 'glassMove 1s linear';
-});
+});*/
+
+
+
+
+//Preloader
+
+/*const preloader = document.querySelector('.preloader');
+
+window.onload = ()=>{ setTimeout(()=>{
+  preloader.style.display = 'none';
+}, 400); */
